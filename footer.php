53457436f46c43b767892f7fad8ed0b1dc756ac0
Eskilop <eskilop@eskilop.it>
2019-03-08 21:24:43 +0100

    Styled and added footer

--- footer.php
+++ footer.php
@@ -1,3 +1,14 @@
 </div>
-<footer>
+</div>
+<?php wp_footer(); ?>
+</body>
+<footer class="footer">
+  <div class="content has-text-centered">
+    <p>
+      <? bloginfo('name'); ?> powered by <strong>Esky</strong> theme, w/ ❤️ by 
+      <a href="https://eskilop.it">
+        <img src="https://assets.gitlab-static.net/uploads/-/system/user/avatar/764306/avatar.png" width="24" height="24">
+      </a>
+    </p>
+  </div>
 </footer>
