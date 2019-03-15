93dd823c0b81641604b877947730407f94028139
Eskilop <eskilop@eskilop.it>
2019-03-15 12:56:03 +0100

    fixed navbar even for entries and styled pages and articles

--- footer.php
+++ footer.php
@@ -3,7 +3,7 @@
 <footer class="footer has-text-centered">
   <div class="content">
     <p>
-      <? bloginfo('name'); ?> powered by <strong>Esky</strong> theme, w/ ❤️ by 
+      Proudly powered by <strong>Esky</strong> theme, w/ ❤️ by 
       <a href="https://eskilop.it">
         <img id="avatar" src="https://assets.gitlab-static.net/uploads/-/system/user/avatar/764306/avatar.png" width="24" height="24">
       </a>
