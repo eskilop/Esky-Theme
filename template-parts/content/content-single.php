5a2f525ba191380c2ade8b7829336f4f8ba0cf92
Eskilop <eskilop@eskilop.it>
2019-03-26 21:21:43 +0100

    added tags support

--- template-parts/content/content-single.php
+++ template-parts/content/content-single.php
@@ -26,19 +26,26 @@
 
 		wp_link_pages(
 			array(
-				'before' => '<div class="page-links">' . __( 'Pages:', 'twentynineteen' ),
+				'before' => '<div class="page-links">' . __( 'Pages:', 'esky' ),
 				'after'  => '</div>',
 			)
 		);
 		?>
-	</div><!-- .entry-content -->
 
-	<footer class="entry-footer">
-		
-	</footer><!-- .entry-footer -->
+		<?php
+			$tags = get_the_tags();
+			$html = '<h4>Tags:</h4><div class="post_tags">';
+			foreach ( $tags as $tag ) {
+				$tag_link = get_tag_link( $tag->term_id );
+
+				$html .= "<span class=\"tag\" class='{$tag->slug}' style='margin: 0.2rem;'><a href='{" . esc_url( $tag_link) . "}'>$tag->name</a></span>";
+			}
+			$html .= '</div>';
+			echo $html;
+		?>
+	</div><!-- .entry-content -->
 
 	<?php if ( ! is_singular( 'attachment' ) ) : ?>
 		<?php get_template_part( 'template-parts/post/author', 'bio' ); ?>
 	<?php endif; ?>
-
 </article><!-- #post-${ID} -->
