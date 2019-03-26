94071b46c08f89d2be6638043ff4391ebabdda34
Eskilop <eskilop@eskilop.it>
2019-03-26 22:42:37 +0100

    don't show tags if none is present

--- inc/articles.php
+++ inc/articles.php
@@ -1,13 +1,15 @@
 <?php
 function esky_display_tags() {
     $tags = get_the_tags();
-	$html = '<h4>Tags:</h4><div class="post_tags">';
-	foreach ( $tags as $tag ) {
-        $tag_link = get_tag_link( $tag->term_id );
-        $html .= "<span class=\"tag\" class='{$tag->slug}' style='margin: 0.2rem;'><a href='{" . esc_url( $tag_link) . "}'>$tag->name</a></span>";
-	}
-	$html .= '</div>';
-	echo $html;
+    if ($tags !== false) {
+        $html = '<h4>Tags:</h4><div class="post_tags">';
+        foreach ( $tags as $tag ) {
+            $tag_link = get_tag_link( $tag->term_id );
+            $html .= "<span class=\"tag\" class='{$tag->slug}' style='margin: 0.2rem;'><a href='{" . esc_url( $tag_link) . "}'>$tag->name</a></span>";
+        }
+        $html .= '</div>';
+        echo $html;
+    }
 }
 
 function esky_display_article_meta() {
