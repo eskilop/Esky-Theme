94b7a2940bf629c267d957527eded0ebedc11dc5
Eskilop <eskilop@eskilop.it>
2019-03-27 20:48:55 +0100

    correct tags spacing

--- inc/articles.php
+++ inc/articles.php
@@ -2,7 +2,7 @@
 function esky_display_tags() {
     $tags = get_the_tags();
     if ($tags !== false) {
-        $html = '<h4>Tags:</h4><div class="post_tags">';
+        $html = '<h4>Tags:</h4><div class="post-tags">';
         foreach ( $tags as $tag ) {
             $tag_link = get_tag_link( $tag->term_id );
             $html .= "<span class=\"tag\" class='{$tag->slug}' style='margin: 0.2rem;'><a href='{" . esc_url( $tag_link) . "}'>$tag->name</a></span>";
