b31032bbc81a88762bc829726537ed2ad4d2dbdd
Eskilop <eskilop@eskilop.it>
2019-06-05 21:15:16 +0200

    makes footer.php get avatar from gravatar

--- footer.php
+++ footer.php
@@ -4,7 +4,7 @@
     <p>
       Proudly powered by <strong>Esky</strong> theme, w/ ❤️ by 
       <a href="https://eskilop.it/so-here-i-am">
-        <img id="avatar" src="https://assets.gitlab-static.net/uploads/-/system/user/avatar/764306/avatar.png" width="24" height="24">
+        <img id="avatar" src="https://secure.gravatar.com/avatar/7cf44f708e5e4578360eb8a971aaa8fe?s=512&d=identicon" width="24" height="24">
       </a>
     </p>
     <div class="esky-social-menu">
